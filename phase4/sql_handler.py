3a5ae1bfc0fbcbe781c32d48e008a7be45f04e4e
Varun Chhangani <varunchhangani@gmail.com>
2019-11-12 18:16:59 +0530

    decode hash to utf8

--- phase4/sql_handler.py
+++ phase4/sql_handler.py
@@ -18,7 +18,7 @@ class color:
 def get_hashed_password(plain_text_password):
     # Hash a password for the first time
     #   (Using bcrypt, the salt is saved into the hash itself)
-    return bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
 def check_password(plain_text_password, hashed_password):
     # Check hashed password. Using bcrypt, the salt is saved into the hash itself
